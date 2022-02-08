4823f897fd91b3b2c55b5b8ece408a3574cbbd56
tealjapa <teemu.a.j.paloniemi@student.jyu.fi>
2022-02-08 14:11:45 +0200

    vaihe 3 parannuksia

--- ba/src/ba/BaGUIController.java
+++ ba/src/ba/BaGUIController.java
@@ -13,7 +13,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextArea;
 /**
  * @author tealjapa
  * @version 24.1.2022
